5ca6f76f56913ffe620cd762285bbccf4439634a
Roman Kirillov <roman.kirillov.06@mail.ru>
2026-08-01 15:04:50 +0300

    add: tests

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,22 +1,32 @@
-import { Post, PostsService } from './posts.service';
+import { Post, PostsService } from "./posts.service";
 
-describe('PostsService', () => {
+describe("PostsService", () => {
   let postsService: PostsService;
-  const post: Omit<Post, 'id' | 'date'> = {
-    text: 'Mocked post',
+
+  const post: Omit<Post, "id" | "date"> = {
+    text: "Mocked post",
   };
 
   beforeEach(async () => {
     postsService = new PostsService();
 
-    postsService.create({ text: 'Some pre-existing post' });
+    postsService.create({ text: "Some pre-existing post" });
   });
 
-  it('should add a new post', () => {
-    // реализуйте тест-кейс
+  it("should add a new post", () => {
+    const createdPost = postsService.create(post);
+
+    expect(createdPost).toBeDefined();
+    expect(createdPost.text).toBe(post.text);
+    expect(createdPost.id).toBe("2"); // первый пост уже создан в beforeEach
+    expect(createdPost.date).toEqual(expect.any(String));
   });
 
-  it('should find a post', () => {
-    // реализуйте тест-кейс
+  it("should find a post", () => {
+    const createdPost = postsService.create(post);
+
+    const foundPost = postsService.find(createdPost.id);
+
+    expect(foundPost).toEqual(createdPost);
   });
 });
