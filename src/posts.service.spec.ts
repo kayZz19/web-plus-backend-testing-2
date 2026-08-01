ad3b8dff8d75ded9dda949cc9941575baafd84e6
Roman Kirillov <roman.kirillov.06@mail.ru>
2026-08-01 15:15:31 +0300

    fix

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,26 +1,26 @@
-import { Post, PostsService } from './posts.service';
+import { Post, PostsService } from "./posts.service";
 
-describe('PostsService', () => {
+describe("PostsService", () => {
   let postsService: PostsService;
 
-  const post: Omit<Post, 'id' | 'date'> = {
-    text: 'Mocked post',
+  const post: Omit<Post, "id" | "date"> = {
+    text: "Mocked post",
   };
 
   beforeEach(() => {
     postsService = new PostsService();
 
-    postsService.create({ text: 'Some pre-existing post' });
+    postsService.create({ text: "Some pre-existing post" });
   });
 
-  it('should add a new post', () => {
+  it("should add a new post", () => {
     const createdPost = postsService.create(post);
 
-    expect(postsService['posts']).toHaveLength(2);
-    expect(postsService['posts']).toContainEqual(createdPost);
+    expect(postsService["posts"]).toHaveLength(2);
+    expect(postsService["posts"]).toContainEqual(createdPost);
   });
 
-  it('should find a post', () => {
+  it("should find a post", () => {
     const createdPost = postsService.create(post);
 
     expect(postsService.find(createdPost.id)).toEqual(createdPost);
